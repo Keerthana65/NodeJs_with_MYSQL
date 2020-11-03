74f257a2bab1873e9bbe855fe73269fe8bbf79c3
Keerthana65 <geerthana65@gmail.com>
2020-11-03 10:50:09 +0530

    changed port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }));
